6e66ab9fc575003cc219fcbc7f44bd2ab2417ad5
kar chen <user@KCCHAN3012>
2016-12-15 16:17:49 +0800

    [#136105147] [#136104959] [#136105073]

---
+++ RSD Gallery/Artbeat/Site.Master.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Linq;
+using Artbeat.Models;
+
+
+namespace Artbeat
+{
+    public partial class SiteMaster : MasterPage
+    {
+
+        
+
+        protected void master_Page_PreLoad(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+       
+
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new Artbeat.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
+        }
+
+        protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
+        {
+           
+        }
+    }
+}
