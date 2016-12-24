85d20193126e9034099139745fb716197aef4224
benjamin <ben920809@gmail.com>
2016-12-24 13:15:32 +0800

    [#136105435] validation and payment type updated

---
+++ RSD Gallery/packages/Artbeat/Account/Register.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using Artbeat.Models;
+
+namespace Artbeat.Account
+{
+    public partial class Register : Page
+    {
+        protected void CreateUser_Click(object sender, EventArgs e)
+        {
+            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
+            IdentityResult result = manager.Create(user, Password.Text);
+            if (result.Succeeded)
+            {
+                IdentityHelper.SignIn(manager, user, isPersistent: false);
+
+                
+
+                IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+            }
+            else 
+            {
+                ErrorMessage.Text = result.Errors.FirstOrDefault();
+            }
+        }
+    }
+}
